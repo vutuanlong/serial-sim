7c07e2631073f054bfdff3e96af6739f6c126ccb
longvt <vutuanlongdhcn@gmail.com>
2025-09-30 00:04:06 +0700

    File import serial chỉ gồm: ngày nhập serial, serial sim, nhà mạng

--- src/Serial/Import.php
+++ src/Serial/Import.php
@@ -30,7 +30,6 @@ class Import {
 		// Form upload file
 		echo '<form method="post" enctype="multipart/form-data">';
 		wp_nonce_field( 'import_serial_nonce', 'import_serial_nonce_field' );
-		echo '<input type="file" name="import_file" accept=".xlsx" required />';
 
 		?>
 		<p>
@@ -51,16 +50,9 @@ class Import {
 				<option value="Wintel">Wintel</option>
 			</select>
 		</p>
-
-		<p>
-			<label>Chọn loại sim:</label><br>
-			<select name="loai_sim" required>
-				<option value="Trả trước">Trả trước</option>
-				<option value="Trả sau">Trả sau</option>
-			</select>
-		</p>
 		<?php
 
+		echo '<input type="file" name="import_file" accept=".xlsx" required />';
 		submit_button( 'Upload và Nhập Serial' );
 		echo '</form>';
 
@@ -78,13 +70,19 @@ class Import {
 
 			$ngay_nhap = sanitize_text_field( $_POST['ngay_nhap'] );
 			$nha_mang  = sanitize_text_field( $_POST['nha_mang'] );
-			$loai_sim  = sanitize_text_field( $_POST['loai_sim'] );
 
 			for ( $row = 2; $row <= $highestRow; $row++ ) {
 				$date = trim( $sheet->getCell( 'B' . $row )->getValue() );
 				$name = trim( $sheet->getCell( 'C' . $row )->getValue() );
 
 				if ( empty( $name ) && empty( $date ) ) {
+					continue;
+				}
+
+				// check nếu serial đã tồn tại (title post type serial)
+				$exists = get_page_by_title( $name, OBJECT, 'serial' );
+				if ( $exists ) {
+					$errors[] = $name;
 					continue;
 				}
 
@@ -95,18 +93,8 @@ class Import {
 				] );
 
 				if ( $post_id ) {
-
 					update_post_meta( $post_id, 'ngay_nhap', $ngay_nhap );
-					update_post_meta( $post_id, 'sdt', trim( $sheet->getCell( 'D' . $row )->getValue() ) );
-					update_post_meta( $post_id, 'sdt_chamdinhdang', trim( $sheet->getCell( 'E' . $row )->getValue() ) );
-					update_post_meta( $post_id, 'dinh_dang_sim', trim( $sheet->getCell( 'F' . $row )->getValue() ) );
 					update_post_meta( $post_id, 'nha_mang', $nha_mang );
-					update_post_meta( $post_id, 'loai_sim', $loai_sim );
-					update_post_meta( $post_id, 'cam_ket', trim( $sheet->getCell( 'I' . $row )->getValue() ) );
-					update_post_meta( $post_id, 'goi_cuoc', trim( $sheet->getCell( 'J' . $row )->getValue() ) );
-					update_post_meta( $post_id, 'kenh_ban', trim( $sheet->getCell( 'K' . $row )->getValue() ) );
-					update_post_meta( $post_id, 'tinh_trang_ban', trim( $sheet->getCell( 'L' . $row )->getValue() ) );
-					update_post_meta( $post_id, 'ghi_chu', trim( $sheet->getCell( 'M' . $row )->getValue() ) );
 				}
 			}
 
